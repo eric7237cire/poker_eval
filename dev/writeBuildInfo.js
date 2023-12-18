a91450d9e900839cdd64141d60296bea891f80b2
eric7237cire <eric7237cire@hotmail.fr>
2023-12-18 17:32:43 +0100

    f

--- dev/writeBuildInfo.js
+++ dev/writeBuildInfo.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path'); 
-const { format } = require(path.join(__dirname, '../vue-poker/node_modules', 'date-fns'));
 
 // Get the current date and time in ISO format
-const currentDate = format(new Date(), 'yyyy/MM/dd HH:mm:ss');
+const currentDate = new Date();
 
-const envVariableValue = process.env.GITHUB_SHA || 'Local';
+const githubSha = process.env.GITHUB_SHA || 'Local';
 
+
+const bi = {
+    build_date: currentDate.getTime(),
+    github_sha: githubSha,
+}
 // Create a string with the date, time, and environment variable value
-const content = `Built: ${currentDate}\nCommit Hash: ${envVariableValue}`;
+const content = JSON.stringify(bi);
 
 // Write the content to a text file (e.g., datetime.txt)
 fs.writeFileSync('../vue-poker/public/build_info.txt', content, 'utf-8');
--- vue-poker/src/components/Footer.vue
+++ vue-poker/src/components/Footer.vue
@@ -4,12 +4,20 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+interface BuildInfo {
+    build_date: number,
+    github_sha: string,
+}
+
 const buildInfo = ref<string>('');
 const build_info_url = './build_info.txt';
 //console.log("build_info_url: " + build_info_url);
 const r = await fetch(build_info_url);
 //console.log("r: " + r);
-const text = await r.text();
+const buildInfoJson: BuildInfo = await r.json();
 //console.log("text: " + text);
-buildInfo.value = text;
+const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'narrow' };
+const formattedDate = new Date(buildInfoJson.build_date).toLocaleTimeString('fr-CH', options);
+buildInfo.value = `Built: ${formattedDate}\nCommit Hash: ${buildInfoJson.github_sha}`;
+
 </script>
